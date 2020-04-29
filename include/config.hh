72cbec0d63ef7f2d92adccb0d1866cd2a01a29df
Dhananjai Sharma <dhananjs@ghc66.ghc.andrew.cmu.edu>
2020-04-29 19:47:40 -0400

    Minor changes

---
+++ include/config.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#define NUM_THREADS
+#define NUM_CHANNELS
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#define TIMER
+
+// #define SERIAL
+#define SIMD
