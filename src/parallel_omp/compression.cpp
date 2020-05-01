1508b61ef4f22f8df12e919aad05038f36c033b1
Dhananjai Sharma <dhananjs@ghc55.ghc.andrew.cmu.edu>
2020-04-30 21:32:06 -0400

    feat(compression): DCT parallelization works

--- src/parallel_omp/compression.cpp
+++ src/parallel_omp/compression.cpp
@@ -1,4 +1,3 @@
-#include <sys/time.h>
 #include <cstdlib>
 #include <iostream>
 #include <stdio.h>
@@ -15,7 +14,7 @@
 #include "../../include/stb_image_write.h"
 #include "dequantization.h"
 
-#define SERIAL 0
+#define SERIAL 1
 #if !SERIAL
 #define OMP
 #endif
@@ -29,7 +28,7 @@ using pixel_t = uint8_t;
 
 int n, m;
 
-void dct(float **DCTMatrix, float **Matrix, int N, int M);
+void dct2(float **DCTMatrix, float **Matrix, int N, int M, int, int);
 void write_mat(FILE *fp, float **testRes, int N, int M);
 void free_mat(float **p);
 void divideMatrix(float **grayContent, int dimX, int dimY, int n, int m);
@@ -54,56 +53,26 @@ void print_mat(float **m, int N, int M){
 
 
 void divideMatrix(float **grayContent, int dimX, int dimY, int n, int m) {
-    float **smallMatrix = calloc_mat(dimX, dimY);
-    float **DCTMatrix = calloc_mat(dimX, dimY);
-
-#if !SERIAL
-
-#endif
-    for (int i = 0; i < n; i += dimX) {
-        for (int j = 0; j < m; j += dimY) {
-#if !SERIAL
-#ifdef OMP
-            // #pragma omp parallel for
-#endif
-#endif
-            for (int k = i; k < i + pixel && k < n; k++) {
-                for (int l = j; l < j + pixel && l < m; l++) {
-                    smallMatrix[k-i][l-j] = grayContent[k][l];
-                }
-            }
-
-            dct(DCTMatrix, smallMatrix, dimX, dimY);
-
-            for (int k = i; k < i + pixel && k < n ;k++) {
-                for (int l=j; l<j+pixel && l<m ;l++) {
-                    globalDCT[k][l] = DCTMatrix[k-i][l-j];
-                }
-            }
-        }
+    if (grayContent == nullptr || globalDCT == nullptr) {
+        cout << "Invalid argument" << endl;
     }
 
-    // TODO: I think the memory is not being freed properly for double pointers
-    delete[] smallMatrix;
-    delete[] DCTMatrix;
+#if !SERIAL
+#ifdef OMP
+#pragma omp parallel for schedule(runtime)
+#endif
+#endif
+    for (int i = 0; i < n; i += dimX) {
+        // cout << "Thread: " << endl;
+        for (int j = 0; j < m; j += dimY) {
+            dct2(globalDCT, grayContent, dimX, dimY, i, j);
+        }
+    }
 }
 
 
-void dct(float **DCTMatrix, float **Matrix, int N, int M) {
-    int i, j, u, v;
-    // for all pixels in the 8 x 8 block
-// #ifdef OMP
-//     #pragma omp parallel for schedule(runtime)
-// cout << getenv(OMP_STACKSIZE) << endl;
-// #endif
-// #pragma omp parallel
-
-// #if !SERIAL
-// #ifdef OMP
-// #pragma omp parallel
-// #endif
-// #endif
-{
+void dct2(float **globalDCT, float **grayMatrix, int N, int M, int offsetX, int offsetY) {
+    int u, v, x, y;
     float cos1, cos2, temp;
     // NOTE: assume that N and M are same.
     float term1 = M_PI / (float)N;
@@ -111,18 +80,13 @@ void dct(float **DCTMatrix, float **Matrix, int N, int M) {
     float one_by_root_2 = 1.0 / sqrt(2);
     float one_by_root_2N = 1.0 / sqrt(2 * N);
 
-// #if !SERIAL
-// #ifdef OMP
-//     #pragma omp for
-// #endif
-// #endif
     for (u = 0; u < N; ++u) {
         for (v = 0; v < M; ++v) {
-            for (i = 0; i < N; i++) {
-                for (j = 0; j < M; j++) {
-                    cos1 = cos(term1 * (i + 0.5) * u);
-                    cos2 = cos(term2 * (j + 0.5) * v);
-                    temp += Matrix[i][j] * cos1 * cos2;
+            for (x = 0; x < N; x++) {
+                for (y = 0; y < M; y++) {
+                    cos1 = cos(term1 * (x + 0.5) * u);
+                    cos2 = cos(term2 * (y + 0.5) * v);
+                    temp += grayMatrix[x + offsetX][y + offsetY] * cos1 * cos2;
                 }
             }
 
@@ -135,12 +99,11 @@ void dct(float **DCTMatrix, float **Matrix, int N, int M) {
                 temp *= one_by_root_2;
             }
 
-            // TODO: Should be round the value?
-            DCTMatrix[u][v] = temp;
+            // TODO: ensure that u + offset < i + pixel and < n
+            // #pragma omp atomic
+            globalDCT[u + offsetX][v + offsetY] = temp;
         }
     }
-}
-
 }
 
 
@@ -151,6 +114,9 @@ void compress(pixel_t *img, int num, int width, int height) {
     int add_rows = (pixel - (n % pixel) != pixel ? pixel - (n % pixel) : 0);
     int add_columns = (pixel - (m % pixel) != pixel ? pixel - (m % pixel) : 0) ;
     float **grayContent = calloc_mat(n + add_rows, m + add_columns);
+    globalDCT = calloc_mat(n + add_rows, m + add_columns);
+    // globalDCT = vector<vector<float>>(n, vector<float>(m));
+
     int dimX = pixel, dimY = pixel;
 
 #if !SERIAL
@@ -231,13 +197,16 @@ bool imagesAreIdentical(pixel_t *img1, pixel_t *img2, int width, int height, int
 
 int main(int argc, char **argv) {
     FILE *fp;
-    fp=fopen("./info.txt","a+"); 
+    fp = fopen("./info.txt","a+"); 
     omp_set_num_threads(NUM_THREADS);
-    string str="../../images/";
-    string ext=".jpg";
+    // TODO: Check if dir exist
+    string img_dir = "../../images/";
+    string save_dir = "./compressed_images/";
+    string ext = ".jpg";
 
-    string path = str + argv[1] + ext;
-    cout << path << endl;
+    string img_name = argv[1] + ext;
+    string path = img_dir + img_name;
+    cout << img_name << ", ";
 
 #ifdef OMP
     cout << "OMP, ";
@@ -252,11 +221,15 @@ int main(int argc, char **argv) {
     compress(img, 0, width, height);
     auto end = chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff_parallel = end - start;
+    cout << "Width: " << width << ", ";
+    cout << "Height: " << height << ", ";
 
 #if SERIAL
-    stbi_write_jpg("sample_ser.jpg", width, height, bpp, img, width * bpp);
+    string save_img = save_dir + "ser_" + img_name;
+    stbi_write_jpg(save_img.data(), width, height, bpp, img, width * bpp);
 #else
-    stbi_write_jpg("sample_par.jpg", width, height, bpp, img, width * bpp);
+    string save_img = save_dir + "par_" + img_name;
+    stbi_write_jpg(save_img.data(), width, height, bpp, img, width * bpp);
 #endif
     stbi_image_free(img);
 
