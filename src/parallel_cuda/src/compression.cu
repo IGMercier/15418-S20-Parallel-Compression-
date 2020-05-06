c67cc98a04e7b076cb7b0e5c585323a73ceaaaf6
Dhananjai Sharma <dhananjs@ghc66.ghc.andrew.cmu.edu>
2020-05-06 11:24:07 -0400

    Minor changes

--- src/parallel_cuda/src/compression.cu
+++ src/parallel_cuda/src/compression.cu
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-#include <string>
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <driver_functions.h>
 
 #include "../include/config_cuda.hh"
-// #include "../../../include/stb_image.h"
-// #include "../../../include/stb_image_write.h"
 
-// using namespace std;
-// using pixel_t = uint8_t;
-
-
-// __constant__ int cudaQuantArr[WINDOW_Y][WINDOW_X];
 __constant__ int cudaQuantArr[WINDOW_Y * WINDOW_X];
 uint8_t *cudaImg;
 
 
 __device__
-void discreteCosTransformCuda(int *grayData, float *patchDCT) {
+void discreteCosTransformCuda(const int *grayData, float *patchDCT, const int &linearIdx) {
     // Put these in the read only memory.
     float cos1, cos2, temp;
+    // Useful constansts
     const float term1 = M_PI / (float)WINDOW_X;
     const float term2 = M_PI / (float)WINDOW_Y;
     const float one_by_root_2 = 1.0 / sqrtf(2);
     const float one_by_root_2N = 1.0 / sqrtf(2 * WINDOW_X);
 
     int x, y;
-    temp = 0;
+    temp = 0.0;
     for (y = 0; y < WINDOW_Y; y++) {
         for (x = 0; x < WINDOW_X; x++) {
             // These cosine values can be pre-stored for a (WINDOW_X, WINDOW_Y)
@@ -51,31 +43,27 @@ void discreteCosTransformCuda(int *grayData, float *patchDCT) {
     }
 
     // TODO: ensure that u + offset < i + pixel and < n
-    patchDCT[threadIdx.y * blockDim.x + threadIdx.x] = (int)temp;
+    patchDCT[linearIdx] = (int)temp;
 }
 
 
-__device__
-void quantizeCuda(float *patchDCT, int *quantData) {
+__device__ __inline__
+void quantizeCuda(const float *patchDCT, int *quantData, const int &linearIdx) {
     // TODO: Use a single array for the purpose of quantizing
-    int linearIdx = threadIdx.y * blockDim.x + threadIdx.x;
-    quantData[linearIdx] = (int)round(
-        // (float)patchDCT[linearIdx] / cudaQuantArr[threadIdx.y][threadIdx.x]);
-        (float)patchDCT[linearIdx] / cudaQuantArr[linearIdx]);
+    quantData[linearIdx] = (int)roundf((float)patchDCT[linearIdx]
+                                       / cudaQuantArr[linearIdx]);
 }
 
 
-__device__
-void dequantizeCuda(int *quantData, int *dequantData) {
+__device__ __inline__
+void dequantizeCuda(const int *quantData, int *dequantData, const int &linearIdx) {
     // TODO: Use a single array for the purpose of quantizing and dequantizing
-    int linearIdx = threadIdx.y * blockDim.x + threadIdx.x;
-    // dequantData[linearIdx] = quantData[linearIdx] * cudaQuantArr[threadIdx.y][threadIdx.x];
     dequantData[linearIdx] = quantData[linearIdx] * cudaQuantArr[linearIdx];
 }
 
 
 __device__
-void invDiscreteCosTransformCuda(int *dequantData, int *patchInverseDCT) {
+void invDiscreteCosTransformCuda(const int *dequantData, int *patchInverseDCT, int offset, const int &linearIdx) {
     int x, y;
     float cos1, cos2, temp;
     // Useful constants.
@@ -94,6 +82,10 @@ void invDiscreteCosTransformCuda(int *dequantData, int *patchInverseDCT) {
         temp += 1/2. * (float)dequantData[0 * blockDim.x + x];
     }
 
+    // if (offset == 202944) {
+    //     printf("Partial INV DCT: %f\n", temp);
+    // }
+
     for (y = 1; y < WINDOW_Y; y++) {
         for (x = 1; x < WINDOW_X; x++) {
             cos1 = cosf(term1 * (y + 0.5) * threadIdx.y);
@@ -102,7 +94,14 @@ void invDiscreteCosTransformCuda(int *dequantData, int *patchInverseDCT) {
         }
     }
 
-    patchInverseDCT[threadIdx.y * blockDim.x + threadIdx.x] = temp * term3 * term4;
+    // if (offset == 202944) {
+    //     printf("Partial INV DCT: %d\n", temp);
+    // }
+
+    patchInverseDCT[linearIdx] = temp * term3 * term4;
+    // if (offset == 202944) {
+    //     printf("Partial INV DCT: %d\n", patchInverseDCT[threadIdx.y * blockDim.x + threadIdx.x]);
+    // }
 }
 
 
@@ -154,45 +153,22 @@ void compressCuda(uint8_t *cudaImg, int width, int height) {
         uint8_t *bgrPixel = (uint8_t *) &cudaImg[offset];
         grayData[linearIdx] = (bgrPixel[0] + bgrPixel[1] + bgrPixel[2]) / 3.f;
     } else if (pixelX >= width) {
-        // padding extra columns
         grayData[linearIdx] = 0;
     } else if (pixelY >= height) {
-        // padding extra rows
         grayData[linearIdx] = 0;
     }
 
     __syncthreads();
-    discreteCosTransformCuda(grayData, patchDCT);
-
-    // Might not need this
+    discreteCosTransformCuda(grayData, patchDCT, linearIdx);
+    quantizeCuda(patchDCT, quantData, linearIdx);
+    dequantizeCuda(quantData, dequantData, linearIdx);
+    __syncthreads();
+    invDiscreteCosTransformCuda(dequantData, patchInverseDCT, offset, linearIdx);
     __syncthreads();
 
-    quantizeCuda(patchDCT, quantData);
-    __syncthreads();
-    dequantizeCuda(quantData, dequantData);
-
-    __syncthreads();
-
-    invDiscreteCosTransformCuda(dequantData, patchInverseDCT);
-    __syncthreads();
-
-    if (blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 2 && threadIdx.y == 3) {
-        printf("DCT value: %f\n", patchDCT[linearIdx]);
-        printf("Gray value: %d\n", grayData[linearIdx]);
+    if (pixelX >= width || pixelY >= height) {
+        return;
     }
-
-    // cudaImg[offset + 0] = grayData[linearIdx];
-    // cudaImg[offset + 1] = grayData[linearIdx];
-    // cudaImg[offset + 2] = grayData[linearIdx];
-
-    // cudaImg[offset + 0] = patchDCT[linearIdx];
-    // cudaImg[offset + 1] = patchDCT[linearIdx];
-    // cudaImg[offset + 2] = patchDCT[linearIdx];
-
-    // Write the compressed data back to the original image.
-    // cudaImg[getOffset(width, pixelY, pixelX) + 0] = quantData[linearIdx];
-    // cudaImg[getOffset(width, pixelY, pixelX) + 1] = quantData[linearIdx];
-    // cudaImg[getOffset(width, pixelY, pixelX) + 2] = quantData[linearIdx];
 
     cudaImg[offset + 0] = patchInverseDCT[linearIdx];
     cudaImg[offset + 1] = patchInverseDCT[linearIdx];
@@ -205,20 +181,6 @@ void cudaSetup(uint8_t *img, int width, int height) {
     cudaMalloc(&cudaImg, sizeof(uint8_t) * num);
     cudaMemcpy(cudaImg, img, sizeof(uint8_t) * num, cudaMemcpyHostToDevice);
 
-    // quantArr = new int*[WINDOW_Y];
-    // for (int i = 0; i < WINDOW_Y; i++) {
-    //     quantArr[i] = new int[WINDOW_X];
-    // }
-
-    // int quantArr[WINDOW_Y][WINDOW_X] = {{16, 11, 12, 14, 12, 10, 16, 14},
-    //                                     {13, 14, 18, 17, 16, 19, 24, 40},
-    //                                     {26, 24, 22, 22, 24, 49, 35, 37},
-    //                                     {29, 40, 58, 51, 61, 60, 57, 51},
-    //                                     {56, 55, 64, 72, 92, 78, 64, 68},
-    //                                     {87, 69, 55, 56, 80, 109, 81, 87},
-    //                                     {95, 98, 103, 104, 103, 62, 77, 113},
-    //                                     {121, 112, 100, 120, 92, 101, 103, 99}
-    //                                     };
     int quantArr[WINDOW_Y * WINDOW_X] = {16, 11, 12, 14, 12, 10, 16, 14,
                                          13, 14, 18, 17, 16, 19, 24, 40,
                                          26, 24, 22, 22, 24, 49, 35, 37,
@@ -249,6 +211,4 @@ void cudaFinish(uint8_t *img, int width, int height) {
     size_t num = NUM_CHANNELS * width * height;
     cudaMemcpy(img, cudaImg, sizeof(uint8_t) * num, cudaMemcpyDeviceToHost);
     cudaFree(cudaImg);
-
-    // TODO: Shifting quant arr to the global read-only memory
 }
