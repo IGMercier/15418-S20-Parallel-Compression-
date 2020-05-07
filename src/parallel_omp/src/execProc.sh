7a34b01cd39ba6d2566507bdce94657b0dac7834
Dhananjai Sharma <dhananjs@ghc66.ghc.andrew.cmu.edu>
2020-05-07 09:47:46 -0400

    change(omp): restructured `parallel_omp` directory

---
+++ src/parallel_omp/src/execProc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm info.txt
+g++ compression.cpp -std=c++11 -fopenmp
+a=0
+while [ "$a" -lt 5 ]    # this is loop1
+do
+  ./a.out "$a"
+  a=`expr $a + 1`
+done
+g++ compare.cpp -std=c++11
+./a.out
