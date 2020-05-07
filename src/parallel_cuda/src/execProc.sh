3c9fea2384b6cf531940821d6fe11c2e1bb115b5
Dhananjai Sharma <dhananjs@ghc66.ghc.andrew.cmu.edu>
2020-05-07 00:25:46 -0400

    changes: minor

--- src/parallel_cuda/src/execProc.sh
+++ src/parallel_cuda/src/execProc.sh
@@ -3,7 +3,7 @@ rm info.txt
 make clean
 make all
 a=0
-while [ "$a" -lt 30 ]    # this is loop1
+while [ "$a" -lt 30 ]  # do first 30 images from `images/`
 do
   ./compressor "$a"
   a=`expr $a + 1`
